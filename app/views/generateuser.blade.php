46cd93410a83f78dbd1f7e04f5343b68797448ba
David Glauner <david.glauner@gmail.com>
2014-10-24 12:29:21 -0400

    Edit Read.me2

--- app/views/generateuser.blade.php
+++ app/views/generateuser.blade.php
@@ -9,9 +9,7 @@
 		<div class="jumbotron">
 			@for($i = 0; $i < Input::get('num-users'); $i++)
 			<p>
-				{{ 
-				HTML::image($fakers->imageUrl($width = 100, $height = 100)."?".$i, 'User Image', array('height' => '100px','width' => '100px','class' => 'floatRight')); 
-				}}
+				{{ HTML::image($fakers->imageUrl($width = 100, $height = 100)."?".$i, 'User Image', array('height' => '100px','width' => '100px','class' => 'floatRight')); }}
 				Name:<br/>{{ $fakers->name; }}
 				<br/>
 				Address:<br/>{{ $fakers->streetAddress; }}
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ A web application called Developer's Best Friend which includes a Lorem Ipsum Ge
 * Random User Generator generates fake user info including an image, name, address and options to add a phone number and an email address.
 
 ## Demo
-
+http://screencast.com/t/BXPt4rbt
 
 ## Details for teaching team
 I tried to focus on Laravel as much as possible, to do as much as I could in the framework, as opposed to PHP.
